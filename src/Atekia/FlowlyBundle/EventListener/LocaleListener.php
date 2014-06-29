745e3813ca248a4dfa2512f1cead9a85dcf96f83
Ignacio Cabrerizo Sánchez <ignacio.cabrerizo@atekia.com>
2014-06-29 04:41:55 +0200

    Minor fixes.

--- src/Atekia/FlowlyBundle/EventListener/LocaleListener.php
+++ src/Atekia/FlowlyBundle/EventListener/LocaleListener.php
@@ -8,16 +8,20 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class LocaleListener implements EventSubscriberInterface
 {
+
     private $defaultLocale;
 
     public function __construct($defaultLocale, array $availableLocales)
     {
+
         $this->defaultLocale = $defaultLocale;
         $this->availableLocales = $availableLocales;
+
     }
 
     private static function pickLocale($fallbackLocale, array $preferredLocales = null, array $availableLocales = null)
     {
+
         if (empty($availableLocales)) {
             return $fallbackLocale;
         }
@@ -40,18 +44,24 @@ class LocaleListener implements EventSubscriberInterface
         $availableLocales = array_values(array_intersect($extendedPreferredLocales, $availableLocales));
 
         return isset($availableLocales[0]) ? $availableLocales[0] : $fallbackLocale;
+
     }
 
     public function onKernelRequest(GetResponseEvent $event)
     {
+
         $request = $event->getRequest();
         $request->setLocale($this->pickLocale($this->defaultLocale, $request->getLanguages(), $this->availableLocales));
+
     }
 
     public static function getSubscribedEvents()
     {
+
         return array(
             KernelEvents::REQUEST => array(array('onKernelRequest', 17)),
         );
+
     }
+
 }
